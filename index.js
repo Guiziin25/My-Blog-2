9837af8f5c798484b3ad086e278c189e36445cab
Guilherme <gaandrade25@gmail.com>
2024-07-31 20:25:04 -0300

    CRUD
    CRUD finished and working perfectly

--- index.js
+++ index.js
@@ -21,6 +21,7 @@
         app.use((req, res, next) => {
             res.locals.success_msg = req.flash("success_msg")
             res.locals.error_msg = req.flash("error_msg")
+            next()
         })
     // Body Parser
         app.use(express.urlencoded({extended: true}))
